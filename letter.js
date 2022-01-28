b71326c4bfaae96d2aeae4bd37386af80e76697d
Tasco Silva <tascosilva@192.168.0.118>
2022-01-28 13:41:13 +0100

    Make search by letter page. Create random cocktail on main page.

---
+++ letter.js
@@ -0,0 +1,95 @@
+const letters = document.getElementById('letters');
+const cocktailList = document.getElementById('cocktailList');
+
+
+// create letters for search by letter
+function setLetters() {
+    var alphabet = '';
+    var l;
+    for (var i = 65; 90 >= i; i++) {
+        // don't show buttons for u and x
+        if (i == 85 || i == 88) {
+            continue
+        } else {
+            l = String.fromCharCode(i);
+            alphabet += '<button class="letter" onclick="setUrl(\'' + l + '\');">' + l + '</button>';
+        }
+    }
+    letters.innerHTML = alphabet;
+}
+
+setLetters();
+
+function setUrl(x) {
+    chosenUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${x}`;
+    getCocktails(chosenUrl);
+}
+
+function createCocktailCard(name, img, cat, id) {
+    var cocktailCard = `
+            <div class='card h-100'>
+                <img src=${img} id="cocktailImg" class="card-img-top img-fluid" alt="${name}">
+                <div class="card-body">
+                    <h5 class="card-title" id="cocktailName">${name}</h5>
+                    <small>Category: ${cat}</small>
+                </div>
+                <div class="text-center mb-4">
+                    <a data-drinkId="${id}" class="btn btnCocktail">Make Cocktail</a>
+                </div>
+            </div>
+        `
+
+    return cocktailCard;
+}
+
+function getCocktails(chosenUrl) {
+    fetch(chosenUrl)
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
+
+                // Examine the text in the response
+                response.json().then(function (data) {
+                    displayCocktails(data);
+                });
+            }
+        )
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        });
+}
+
+function displayCocktails(cocktail) {
+    cocktailList.innerHTML = '';
+    var drinks = cocktail.drinks;
+    var cocktailCards = [];
+
+    for (let i = 0; i < 9; i++) {
+        var drink = drinks[i];
+        var name = drink.strDrink;
+        var img = drink.strDrinkThumb;
+        var cat = drink.strCategory;
+        var id = drink.idDrink;
+        var cocktailCard = createCocktailCard(name, img, cat, id);
+
+        cocktailCards.push(cocktailCard);
+
+    }
+    // loop over each cocktail and append it to 'cocktailList'
+    cocktailCards.map(cocktailCard => {
+        var z = document.createElement('div');
+        z.classList.add('col');
+        z.innerHTML = cocktailCard;
+        cocktailList.appendChild(z);
+    })
+}
+
+$(document).on("click", ".btnCocktail", function (e) {
+    id = e.target.getAttribute("data-drinkId");
+    localStorage.setItem('drinkId', id);
+    location.href = "cocktail.html";
+})
--- script.js
+++ script.js
@@ -3,50 +3,8 @@ const url_name = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=marga
 const url_first = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
 const url_id = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007'
 
-const letters = document.getElementById('letters');
+
 const cocktailList = document.getElementById('cocktailList');
-
-// create letters for search by letter
-function setLetters() {
-    var alphabet = '';
-    var l;
-    for (var i = 65; 90 >= i; i++) {
-        // don't show buttons for u and x
-        if (i == 85 || i == 88) {
-            continue
-        } else {
-            l = String.fromCharCode(i);
-            alphabet += '<button class="letter" onclick="setUrl(\'' + l + '\');">' + l + '</button>';
-        }
-    }
-    letters.innerHTML = alphabet;
-}
-
-setLetters();
-
-
-function setUrl(x) {
-    chosenUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${x}`;
-    getCocktails(chosenUrl);
-}
-
-
-function createCocktailCard(name, img, cat, id) {
-    var cocktailCard = `
-            <div class='card h-100'>
-                <img src=${img} id="cocktailImg" class="card-img-top img-fluid" alt="${name}">
-                <div class="card-body">
-                    <h5 class="card-title" id="cocktailName">${name}</h5>
-                    <small>Category: ${cat}</small>
-                </div>
-                <div class="text-center mb-4">
-                    <a data-drinkId="${id}" class="btn btnCocktail">Make Cocktail</a>
-                </div>
-            </div>
-        `
-
-    return cocktailCard;
-}
 
 function getRandomCocktail() {
     fetch(url_random)
@@ -69,107 +27,52 @@ function getRandomCocktail() {
         });
 }
 
+function createSingleCocktailCard(name, img, cat, alc, glass, ingredients, steps) {
+    var cocktailCard =
+        `
+        <h3 class="card-title">${name}</h3>
+        <div class="card mb-3"">
+            <div class="row align-items-center g-0">
+                <div class="col-md-4">
+                    <img src=${img} class="img-fluid rounded-start" alt=${name}>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">           
+                        <div class="info">
+                            <p><i class="fas fa-tag"></i> Category: <span>${cat}</span></p>
+                            <p><i class="fas fa-wine-bottle"></i> Alcholic: <span>${alc}</span></p>
+                            <p><i class="fas fa-glass-martini-alt"></i> Glass: <span>${glass}</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-5" id="ingredient">
+                    <h4>Ingredients</h4>
+                    <ul>${ingredients}</ul>
+                </div>
+                <div class="col-md-7" id="step">
+                    <h4>Steps</h4>
+                    <ol>${steps}</ol>
+                </div>
+            </div>
+        </div>`
+    return cocktailCard;
+}
+
 function displayRandomCocktail(cocktail) {
-    let drink = cocktail.drinks[0];
-
-    cocktailName.innerHTML = drink.strDrink;
-    img.src = drink.strDrinkThumb;
-
-    for (let i = 1; i < 16; i++) {
-
-        if (drink[`strIngredient${i}`] === null || drink[`strIngredient${i}`] === '') {
-            break;
-        }
-
-        if (drink[`strMeasure${i}`] === null) {
-            drink[`strMeasure${i}`] = '';
-        }
-
-        let item = document.createElement('li');
-        item.innerHTML = drink[`strMeasure${i}`] + 'of ' + drink[`strIngredient${i}`];
-
-        ingredient.appendChild(item);
-    }
-
-    instruction.innerHTML = drink.strInstructions;
-}
-
-function getCocktails(chosenUrl) {
-    fetch(chosenUrl)
-        .then(
-            function (response) {
-                if (response.status !== 200) {
-                    console.log('Looks like there was a problem. Status Code: ' +
-                        response.status);
-                    return;
-                }
-
-                // Examine the text in the response
-                response.json().then(function (data) {
-                    displayCocktails(data);
-                });
-            }
-        )
-        .catch(function (err) {
-            console.log('Fetch Error :-S', err);
-        });
-}
-
-function displayCocktails(cocktail) {
-    cocktailList.innerHTML = '';
-    var drinks = cocktail.drinks;
-    var cocktailCards = [];
-
-    for (let i = 0; i < 9; i++) {
-        var drink = drinks[i];
-        var name = drink.strDrink;
-        var img = drink.strDrinkThumb;
-        var cat = drink.strCategory;
-        var id = drink.idDrink;
-        var cocktailCard = createCocktailCard(name, img, cat, id);
-
-        cocktailCards.push(cocktailCard);
-
-    }
-    // loop over each cocktail and append it to 'cocktailList'
-    cocktailCards.map(cocktailCard => {
-        var z = document.createElement('div');
-        z.classList.add('col');
-        z.innerHTML = cocktailCard;
-        cocktailList.appendChild(z);
-    })
-}
-
-function getSingleCocktail(chosenUrl) {
-    fetch(chosenUrl)
-        .then(
-            function (response) {
-                if (response.status !== 200) {
-                    console.log('Looks like there was a problem. Status Code: ' +
-                        response.status);
-                    return;
-                }
-
-                // Examine the text in the response
-                response.json().then(function (data) {
-                    displayCocktail(data);
-                });
-            }
-        )
-        .catch(function (err) {
-            console.log('Fetch Error :-S', err);
-        });
-}
-
-function displaySingleCocktail(cocktail) {
     cocktailList.innerHTML = '';
     var drinks = cocktail.drinks;
     var drink = drinks[0];
     var name = drink.strDrink;
     var img = drink.strDrinkThumb;
-    var steps = drink.strInstructions;
+    var cat = drink.strCategory;
+    var alcoholic = drink.stringAlcoholic;
+    var alc = alcoholic === 'Alcoholic' ? 'Yes' : 'No';
+    var glass = drink.Glass;
     var ingredients = "";
 
+    // create measure and ingredient loop
     for (let j = 1; j < 16; j++) {
         // Stop loop when the ingredients are null or empty
         if (drink[`strIngredient${j}`] === null || drink[`strIngredient${j}`] === '') {
@@ -181,18 +84,26 @@ function displaySingleCocktail(cocktail) {
             }
             var ingredient = drink[`strIngredient${j}`];
             var measure = drink[`strMeasure${j}`];
-            ingredients += `<li>${measure} of ${ingredient}</li>`;
+            ingredients += `<li><input type="checkbox" /> ${measure} of ${ingredient}</li>`;
         }
     }
 
-    var cocktailCard = createSingleCocktailCard(name, img, ingredients, steps);
+    // create instructions
+    var instruction = drink.strInstructions;
+    var instructions = instruction.split(". ");
+    // remove the period from the last element of instruction
+    var oldinstr = instructions[instructions.length - 1];
+    var newinstr = oldinstr.substring(0, oldinstr.length - 1);
+    instructions[instructions.length - 1] = newinstr;
 
-    cocktailList.appendChild(cocktailCard);
+    var steps = ``;
+    instructions.forEach((step) => {
+        steps += `<li>${step}.</li>`
+    })
+
+    var cocktailCard = createSingleCocktailCard(name, img, cat, alc, glass, ingredients, steps);
+
+    cocktailList.innerHTML = cocktailCard;
 }
 
-
-$(document).on("click", ".btnCocktail", function (e) {
-    id = e.target.getAttribute("data-drinkId");
-    localStorage.setItem('drinkId', id);
-    location.href = "cocktail.html";
-})
+getRandomCocktail();
